1907b0b2ba175ff9215422e2d386160cc56b4e03
Jisoo Yoon <jyoon006@gmail.com>
2016-02-08 18:28:20 -0500

    added initialize function to has the user pw using bcrypt

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,32 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 var User = db.Model.extend({
+  defaults: {
+    username: "",
+    password: ""
+  },
+
+  links: function() {
+    return this.hasMany(Link);
+  },
+
+  initialize: function() {
+    this.on('creating', function(model, attrs, options) {
+      bcrypt.hash(this.password, 'saltpepper', null, function(err, hash) {
+        if(err) {
+          console.log('error while hasing the password: ' + err);
+        }
+        model.set('code', hash);
+      });
+    });
+  }
+  // login: function() {
+
+  // },
+  // signup: function() {
+
+  // }
+ 
 });
 
 module.exports = User;
